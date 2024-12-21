aaa6588bb3e9255781521fa9345af4f1c2037da0
Alexander Tsoi <tsoialexander2005@gmail.com>
2024-12-21 06:13:48 +0600

    2 pages future academy

---
+++ events/events.js
@@ -0,0 +1,7 @@
+let box = document.querySelector('.burger')
+let burger = document.querySelector('.burger__menu')
+
+let toggleClass = ()=>{
+    box.classList.toggle('active')
+    burger.classList.toggle('active')
+}
---
+++ js.js
@@ -0,0 +1,23 @@
+let box = document.querySelector('.toggle-btn')
+let content = document.querySelector('.content')
+
+let toggleClass = ()=>{
+    box.classList.toggle('active')
+    content.classList.toggle('active')
+}
+
+let box1 = document.querySelector('.toggle-btn1')
+let content1 = document.querySelector('.content1')
+
+let toggleClass1 = ()=>{
+    box1.classList.toggle('active')
+    content1.classList.toggle('active')
+}
+
+let box2 = document.querySelector('.toggle-btn2')
+let content2 = document.querySelector('.content2')
+
+let toggleClass2 = ()=>{
+    box2.classList.toggle('active')
+    content2.classList.toggle('active')
+}
